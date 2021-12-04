674cd8985d4c171d9d8ab8156e8b7979628a7780
Juli <julianmenav2@gmail.com>
2021-12-04 22:45:29 +0100

    Added props and their states to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
   const startTrivia = async () => {};
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = () => {};
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true;)
+
   return (
     <div className="App">
       <h1>React Quizz</h1>
@@ -13,7 +23,14 @@ function App() {
       </button>
       <p className="score">score</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard 
+        questionNumber = {number + 1}
+        totalQuestions = {TOTAL_QUESTIONS}
+        question = {questions[number].question}
+        answers = {questions[number].answers}
+        userAnswer= {userAnswers ? userAnswers[number] : undefined}
+        callback= {checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
